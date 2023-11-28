d0da4e3e12b747ff58ca595861bf5f5b8a43e729
c-Alvinn <141906500+c-Alvinn@users.noreply.github.com>
2023-11-28 02:17:28 -0300

    Updating InserirActivity and fixing errors

--- Prova02PDM/app/src/main/java/com/example/prova02pdm/telas/InserirActivity.kt
+++ Prova02PDM/app/src/main/java/com/example/prova02pdm/telas/InserirActivity.kt
@@ -2,6 +2,7 @@ package com.example.prova02pdm.telas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.prova02pdm.R
 import com.example.prova02pdm.databinding.ActivityInserirBinding
@@ -15,57 +16,130 @@ class InserirActivity : AppCompatActivity() {
         binding = ActivityInserirBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.layoutBtn1.setOnClickListener(){
-            binding.popUp1.visibility = View.VISIBLE
+        binding.layoutBtnLocacao.setOnClickListener(){
+            binding.popUpSelect.visibility = View.VISIBLE
 
-            binding.btnInserirImovel.setOnClickListener(){
-
-                val matricula = binding.editTextMatricula.text.toString()
-                val endereco = binding.editTextEndereco.text.toString()
-                val aluguel = binding.editTextAluguel.text.toString().toFloat()
-
+            binding.layoutBtnImovel.setOnClickListener(){
+                imovelPopUp()
             }
-            binding.btnCancelarImovel.setOnClickListener(){
-                binding.popUp1.visibility = View.INVISIBLE
+            binding.layoutBtnProprietario.setOnClickListener(){
+                proprietarioPopUp()
             }
-        }
-
-        binding.layoutBtn2.setOnClickListener(){
-            binding.popUp2.visibility = View.VISIBLE
-
-            binding.btnInserirProprietario.setOnClickListener(){
-
-                val cpf = binding.editTextCPFprop.text.toString()
-                val nome = binding.editTextNomeprop.text.toString()
-                val email = binding.editTextEmail.text.toString()
-
+            binding.layoutBtnInquilino.setOnClickListener(){
+                inquilinoPopUp()
             }
-            binding.btnCancelarProprietario.setOnClickListener(){
-                binding.popUp2.visibility = View.INVISIBLE
+            binding.btnInserir.setOnClickListener(){
+                //valida se tem tudo e joga no banco de dados
+
+                binding.popUpSelect.visibility = View.INVISIBLE
             }
-        }
-
-        binding.layoutBtn3.setOnClickListener(){
-            binding.popUp3.visibility = View.VISIBLE
-
-            binding.btnInserirInquilino.setOnClickListener(){
-
-                val cpf = binding.editTextCPFinq.text.toString()
-                val nome = binding.editTextNomeinq.text.toString()
-                val caucao = binding.editTextCaucao.text.toString().toFloat()
-
+            binding.btnCancelarInsercao.setOnClickListener(){
+                //faz as variáveis zerar
+                binding.popUpSelect.visibility = View.INVISIBLE
             }
-            binding.btnCancelarInquilino.setOnClickListener(){
-                binding.popUp3.visibility = View.INVISIBLE
-            }
-        }
-
-        binding.layoutBtn4.setOnClickListener(){
-
         }
 
         binding.btnBack.setOnClickListener(){
             finish()
+        }
+
+    }
+
+    private fun fecharPopUp(){
+
+        binding.popUpInput.visibility = View.INVISIBLE
+
+        binding.tituloPopUp.text = ""
+        binding.editTextFirst.hint = ""
+        binding.editTextSecond.hint = ""
+        binding.editTextFloat.hint = ""
+        binding.editTextFirst.text.clear()
+        binding.editTextSecond.text.clear()
+        binding.editTextFloat.text.clear()
+
+        if (binding.editTextEmail.visibility == View.INVISIBLE) {
+            binding.editTextEmail.visibility = View.VISIBLE
+        }
+
+        if (binding.editTextFloat.visibility == View.INVISIBLE) {
+            binding.editTextFloat.visibility = View.VISIBLE
+        }
+
+
+    }
+
+    private fun imovelPopUp(){
+
+        binding.popUpInput.visibility = View.VISIBLE
+
+        binding.tituloPopUp.text = "Inserir Imóvel"
+        binding.editTextFirst.hint = "Matrícula"
+        binding.editTextSecond.hint = "Endereço"
+        binding.editTextEmail.visibility = View.INVISIBLE
+        binding.editTextFloat.hint = "Valor do Alguel"
+
+        binding.btnOk.setOnClickListener(){
+            val matricula = binding.editTextFirst.text.toString()
+            val endereco = binding.editTextSecond.text.toString()
+            val aluguel = binding.editTextFloat.text.toString()
+
+            fecharPopUp()
+            // return imovel
+        }
+        binding.btnCancelar.setOnClickListener(){
+            fecharPopUp()
+            // return null
+        }
+
+    }
+
+    private fun proprietarioPopUp(){
+
+        binding.popUpInput.visibility = View.VISIBLE
+
+        binding.tituloPopUp.text = "Inserir Proprietário"
+        binding.editTextFirst.hint = "CPF"
+        binding.editTextSecond.hint = "Nome"
+        binding.editTextFloat.visibility = View.INVISIBLE
+
+        binding.btnOk.setOnClickListener(){
+            val cpf = binding.editTextFirst.text.toString()
+            val nome = binding.editTextSecond.text.toString()
+            val email = binding.editTextEmail.text.toString()
+
+            fecharPopUp()
+            // return proprietario
+        }
+        binding.btnCancelar.setOnClickListener(){
+            fecharPopUp()
+            // return null
+        }
+
+    }
+
+    private fun inquilinoPopUp(){
+
+        binding.popUpInput.visibility = View.VISIBLE
+
+        binding.tituloPopUp.text = "Inserir Inquilino"
+        binding.editTextFirst.hint = "CPF"
+        binding.editTextSecond.hint = "Nome"
+        binding.editTextEmail.visibility = View.INVISIBLE
+        binding.editTextFloat.hint = "Valor do Caução Depositado"
+
+        binding.btnOk.setOnClickListener(){
+            val cpf = binding.editTextFirst.text.toString()
+            val nome = binding.editTextSecond.text.toString()
+            val caucao = binding.editTextFloat.text.toString()
+
+            Log.i("teste", cpf+" "+nome+" "+caucao)
+
+            fecharPopUp()
+            // return inquilino
+        }
+        binding.btnCancelar.setOnClickListener(){
+            fecharPopUp()
+            // return null
         }
 
     }
