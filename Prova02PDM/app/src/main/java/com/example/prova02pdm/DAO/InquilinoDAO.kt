372566b8b2e964888fa5593cb973a6f3a6ed9142
DevGustavus <gustavusinvictus@gmail.com>
2023-11-30 20:49:54 -0300

    atualizando DAO

--- Prova02PDM/app/src/main/java/com/example/prova02pdm/DAO/InquilinoDAO.kt
+++ Prova02PDM/app/src/main/java/com/example/prova02pdm/DAO/InquilinoDAO.kt
@@ -3,6 +3,7 @@ package com.example.prova02pdm.DAO
 import android.content.ContentValues
 import android.util.Log
 import com.example.prova02pdm.banco.MyDataBaseHelper
+import com.example.prova02pdm.classes.Imovel
 import com.example.prova02pdm.classes.Inquilino
 
 class InquilinoDAO(banco : MyDataBaseHelper) {
@@ -26,6 +27,52 @@ class InquilinoDAO(banco : MyDataBaseHelper) {
             return false
         }
         return true
+    }
+
+    fun excluirInquilino(inquilino : Inquilino){
+        val db_exclusao = this.banco.readableDatabase
+        val condicao = "id = "+inquilino.id
+        val confirmaExclusao = db_exclusao.delete("Inquilino", condicao, null)
+        Log.i("Teste","Após a exclusão: "+confirmaExclusao)
+    }
+
+    fun atualizarInquilino(inquilino : Inquilino): Boolean{
+        val db_atualizacao = this.banco.writableDatabase
+        val valores = ContentValues().apply{
+            put("CPF_inq", inquilino.CPF_inq)
+            put("nome", inquilino.nome)
+            put("Valor_depositado", inquilino.Valor_depositado)
+        }
+        val condicao = "id = "+inquilino.id
+        val confirmaAtualizacao = db_atualizacao.update("Inquilino", valores, condicao, null)
+        Log.i("Teste", "Atualização: $confirmaAtualizacao")
+        if (confirmaAtualizacao <= 0){
+            return false
+        }
+        return true
+    }
+
+    fun retornarInquilino(id : Int): Inquilino? {
+        var inquilino: Inquilino? = null
+        val db_leitura = this.banco.readableDatabase
+        val cursor = db_leitura.rawQuery("select * from Inquilino",null)
+        with(cursor) {
+            while (moveToNext()) {
+                val id_BC = getInt(getColumnIndexOrThrow("id"))
+                val CPF_inq = getString(getColumnIndexOrThrow("CPF_inq"))
+                val nome = getString(getColumnIndexOrThrow("nome"))
+                val Valor_depositado = getFloat(getColumnIndexOrThrow("Valor_depositado"))
+                android.util.Log.i("Teste","ID: "+id_BC+" - CPF_inq: "+CPF_inq+ " - Nome: "+nome+ " - Valor_depositado: "+Valor_depositado)
+
+                if(id == id_BC){
+                    android.util.Log.i("Teste","ID: "+id_BC+" - CPF_inq: "+CPF_inq+ " - Nome: "+nome+ " - Valor_depositado: "+Valor_depositado)
+                    inquilino = Inquilino(CPF_inq,nome,Valor_depositado)
+                    break
+                }
+            }
+        }
+        cursor.close()
+        return inquilino
     }
 
     fun retornarUltimoID(): Int{
--- Prova02PDM/app/src/main/java/com/example/prova02pdm/DAO/LocacaoDAO.kt
+++ Prova02PDM/app/src/main/java/com/example/prova02pdm/DAO/LocacaoDAO.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import com.example.prova02pdm.banco.MyDataBaseHelper
 import com.example.prova02pdm.classes.Inquilino
 import com.example.prova02pdm.classes.Locacao
+import com.example.prova02pdm.classes.Proprietario
 
 class LocacaoDAO(banco : MyDataBaseHelper) {
 
@@ -32,6 +33,43 @@ class LocacaoDAO(banco : MyDataBaseHelper) {
             return false
         }
         return true
+    }
+
+    fun excluirLocacao(locacao : Locacao){
+        val db_exclusao = this.banco.readableDatabase
+        val condicao = "id = "+locacao.id
+        val confirmaExclusao = db_exclusao.delete("Locacao", condicao, null)
+        Log.i("Teste","Após a exclusão: "+confirmaExclusao)
+    }
+
+    fun retornarLocacao(id : Int): Locacao? {
+        val daoImovel = ImovelDAO(this.banco)
+        val daoProp = ProprietarioDAO(this.banco)
+        val daoInqui = InquilinoDAO(this.banco)
+
+        var locacao: Locacao? = null
+        val db_leitura = this.banco.readableDatabase
+        val cursor = db_leitura.rawQuery("select * from Locacao",null)
+        with(cursor) {
+            while (moveToNext()) {
+                val id_BC = getInt(getColumnIndexOrThrow("id"))
+                val id_proprietario = getInt(getColumnIndexOrThrow("id_proprietario"))
+                val id_imovel = getInt(getColumnIndexOrThrow("id_imovel"))
+                val id_inquilino = getInt(getColumnIndexOrThrow("id_inquilino"))
+                android.util.Log.i("Teste","ID: "+id_BC+" - id_proprietario: "+id_proprietario+ " - id_imovel: "+id_imovel+ " - id_inquilino: "+id_inquilino)
+
+                if(id == id_BC){
+                    android.util.Log.i("Teste","ID: "+id_BC+" - id_proprietario: "+id_proprietario+ " - id_imovel: "+id_imovel+ " - id_inquilino: "+id_inquilino)
+                    val objProprietario = daoProp.retornarProprietario(id_proprietario)
+                    val objImovel = daoImovel.retornarImovel(id_imovel)
+                    val objInquilino = daoInqui.retornarInquilino(id_inquilino)
+                    locacao = Locacao(objProprietario,objImovel,objInquilino)
+                    break
+                }
+            }
+        }
+        cursor.close()
+        return locacao
     }
 
 }
